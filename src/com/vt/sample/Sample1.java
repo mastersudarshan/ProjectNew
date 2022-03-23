934ab27c83e0db8612a78644b45d7729d5918cbb
mastersudarshan <102040204+mastersudarshan@users.noreply.github.com>
2022-03-23 17:14:56 +0530

    fifth commit

--- src/com/vt/sample/Sample1.java
+++ src/com/vt/sample/Sample1.java
@@ -5,9 +5,8 @@ public class Sample1 {
 	public static void main(String[] args) {
 		
 
-		System.out.println("This is fourth commit");
-
-		System.out.println("This is fourth commit");
+		
+		System.out.println("This is fifth commit");
 
 
 	}
